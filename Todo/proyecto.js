a9a95338993644c86280dde6a6b793c91fb46064
millieloisi <milu.loisi@gmail.com>
2024-08-23 15:07:29 -0300

    Update proyecto.js
    milli 5

--- Todo/proyecto.js
+++ Todo/proyecto.js
@@ -1,19 +1,1 @@
-document.getElementById("Boton").addEventListener("click", function() {
-    this.classList.toggle("green");
-    this.classList.toggle("red");
-});
 
-document.getElementById("Boton1").addEventListener("click", function() {
-    this.classList.toggle("green");
-    this.classList.toggle("red");
-});
-
-document.getElementById("Boton2").addEventListener("click", function() {
-    this.classList.toggle("green");
-    this.classList.toggle("red");
-});
-
-document.getElementById("Boton3").addEventListener("click", function() {
-    this.classList.toggle("green");
-    this.classList.toggle("red");
-});
