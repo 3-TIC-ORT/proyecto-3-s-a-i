7d23564811eeb984d078a41e60b92fa66d64b530
Teo2602 <teogonroz@gmail.com>
2024-08-28 15:58:31 -0300

    IMPORTANTEEEEEEEEE
    cree data.json para guardar la funcion de cada sensor.
    serialport ya andaaaaa vamooooo.
    cree las funciones mute y setsounddevice que se ejecutan en el cmd automaticamente.
    pude extraer el dirname para que la posicion del archivo no interfiera en pcs diferentes.

--- back/proyectoback.js
+++ back/proyectoback.js
@@ -1,26 +1,48 @@
 import { SerialPort } from 'serialport';
-const puerto=new SerialPort({path:'COM4'},{baudRate:9600});
-//import { exec } from 'child_process';
-// exec( `C:/Users/49194525/Documents/GitHub/Proyecto-soporte/nircmd/nircmd.exe mutesysvolume 1`);
+import { fileURLToPath } from 'node:url';
+import { dirname,join } from 'node:path';
+import { exec } from 'child_process';
+import  fs  from 'fs'
 
+const puerto=new SerialPort({path:'COM4',baudRate:9600});
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const nirpath = join(__dirname,"/nircmd/nircmd.exe");
 
-let sensores=['a1','p1','p2','p3','b1','b2','b3']
+function mute(numP){
+    let comand = nirpath+" mutesysvolume "+numP;
+    exec(comand);
+}
+function setdevice(numP){
+    if(numP===1){
+        let comand = nirpath+" setdefaultsounddevice Altavoces";
+        exec(comand)
+    } else if(numP===0){
+        let comand = nirpath+" setdefaultsounddevice Auriculares";
+        exec(comand)
+    }
+}
+
 //crear funciones con las funcionalidades como por ejemplo lo de cambiar el output, volumen, intensidad de
-//la luz, etc
-function CategorizadorS(r){
+//la luz, etc.
+//para resolver el tema de los decivelios en el volumen se me ocurre hacer una lista con los decivelios
+//que representan los numeros del 0 al 100
+function CategorizadorS(txt){
+let sensores=['a1','p1','p2','p3','b1','b2','b3'];
     for(let i=0;i<sensores.length;i++){
-    if(r.includes(sensores[i])){
-    let num=r.replace(`${sensores[i]}`,"").replace("\n","");
-    let Fc=0//aca necesito tomar el valor asignado al sensor en la pagina web
-    console.log(num)
-    }}
+        if(txt.includes(sensores[i])){
+        let sensor=sensores[i]
+        let num=Number(txt.replace(`${sensor}`,"").replace("\n",""));
+        let Fc=JSON.parse(fs.readFileSync('data.json','utf-8'));
+            for(let I=0;I<Fc.length;I++){
+                if(Fc[I].nombre===sensor){
+                Fc[I].function
+            };
+        };
+    };
 };
 
 
 puerto.on('data',function(data){
-    var enc = new TextDecoder();
-    var arr = new Uint8Array(data);
-    ready=enc.decode(arr);
-    console.log(ready)
-    CategorizadorS(ready);
+    let texto=Buffer.from(data, 'hex').toString('utf-8');
+    CategorizadorS(texto);
 });
