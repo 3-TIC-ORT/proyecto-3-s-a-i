e5f03dca54d3995d10dd5281d6684d807c9d091e
Teo2602 <teogonroz@gmail.com>
2024-08-30 15:40:15 -0300

    Importante
    ahora se pueden obtener las funciones asociadas al sensor desde data.json y se pueden ejecutar en node.
    creé las funciones setvolume y setbrightnes que hacen lo que dice el nombre, no las probé pero deberían funcionar.
    mute anda bien.

--- back/proyectoback.js
+++ back/proyectoback.js
@@ -2,40 +2,69 @@ import { SerialPort } from 'serialport';
 import { fileURLToPath } from 'node:url';
 import { dirname,join } from 'node:path';
 import { exec } from 'child_process';
-import  fs  from 'fs'
+import  fs  from 'fs';
 
 const puerto=new SerialPort({path:'COM4',baudRate:9600});
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const nirpath = join(__dirname,"/nircmd/nircmd.exe");
 
+let funciones={
+    mute:mute,
+    setdevice:setdevice,
+    setvolume:setvolume,
+    setbrightnes:setbrightnes
+};
 function mute(numP){
-    let comand = nirpath+" mutesysvolume "+numP;
-    exec(comand);
-}
-function setdevice(numP){
     if(numP===1){
+    let comand = nirpath+" mutesysvolume 2";
+    exec(comand);
+    };
+};
+let setdevicestate=0;
+function setdevice(numP){
+    if(setdevicestate!=numP){
+        if(numP===1){
         let comand = nirpath+" setdefaultsounddevice Altavoces";
-        exec(comand)
-    } else if(numP===0){
+        exec(comand);
+        } else if(numP===0){
         let comand = nirpath+" setdefaultsounddevice Auriculares";
-        exec(comand)
-    }
-}
+        exec(comand);
+        };
+        setdevicestate=numP;
+    };
+};
+let maxvolumen=65535;
+let maxpot=1023;
+let setvolumestate=0;
+function setvolume(numP){
+    if(setvolumestate!=numP){
+    let valorcomand=Number(math.round((numP/maxpot)*maxvolumen));
+    let comand = nirpath+" setsysvolume "+valorcomand;
+    exec(comand);
+    setvolumestate=numP;
+    };
+};
+let setbrightnesstate=0;
+function setbrightnes(numP){
+    if(setbrightnesstate!=numP){
+    let valorcomand=Number(math.round((numP/maxpot)*100));
+    let comand=nirpath+" setbrigtnes "+valorcomand;
+    exec(comand);
+    setbrightnesstate=numP;
+    };
+};
 
-//crear funciones con las funcionalidades como por ejemplo lo de cambiar el output, volumen, intensidad de
-//la luz, etc.
-//para resolver el tema de los decivelios en el volumen se me ocurre hacer una lista con los decivelios
-//que representan los numeros del 0 al 100
 function CategorizadorS(txt){
 let sensores=['a1','p1','p2','p3','b1','b2','b3'];
     for(let i=0;i<sensores.length;i++){
         if(txt.includes(sensores[i])){
-        let sensor=sensores[i]
+        let sensor=sensores[i];
         let num=Number(txt.replace(`${sensor}`,"").replace("\n",""));
         let Fc=JSON.parse(fs.readFileSync('data.json','utf-8'));
             for(let I=0;I<Fc.length;I++){
                 if(Fc[I].nombre===sensor){
-                Fc[I].function
+                funciones[`${Fc[I].funcion}`](num);
+                };
             };
         };
     };
