07e21938503d6746a3ffd143251b7070b3a97d13
Ramiro08ds <Sanchezriverazaied@gmail.com>
2024-09-13 14:09:01 -0300

    Trabajos tinkercad pruebas-Rami

---
+++ hard/dos/dos.ino
@@ -0,0 +1,20 @@
+// C++ code
+//
+int pinLed = 10;
+int pinPot = A0;
+int valPot;
+int luz; 
+void setup()
+{
+  pinMode(pinLed, OUTPUT);
+    pinMode(pinPot, INPUT);
+    Serial.begin(9600);
+}
+
+void loop()
+{ 
+valPot = analogRead(pinPot);
+  Serial.println(valPot);
+  luz = map(valPot,0,1023,0,255);
+  analogWrite(pinLed, luz);
+}
