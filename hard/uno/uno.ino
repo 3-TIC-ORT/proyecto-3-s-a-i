809e42143db8a574f9c2ebdec6506d207d457d5d
Ramiro08ds <Sanchezriverazaied@gmail.com>
2024-10-16 15:53:56 -0300

    Update uno.ino

--- hard/uno/uno.ino
+++ hard/uno/uno.ino
@@ -1,20 +1,33 @@
 int vOut; 
-int pinPot = A0;
-int pinLed = 10;
+#define pinPot A3
+#define pinLed 10
+#define pinPot2 A0
+#define pinLed2 5
 int valPot;
 int luz;
+int valPot2;
+int luz2;
 void setup()
 {
     pinMode(pinLed, OUTPUT);
     pinMode(pinPot, INPUT);
+     pinMode(pinLed2, OUTPUT);
+    pinMode(pinPot2, INPUT);
   Serial.begin(9600);
 }
 
 void loop()
 {
    valPot = analogRead(pinPot);
+   Serial.println("tu luz es:");
    Serial.println(valPot);
    luz = map(valPot,0,1023,0,255);
    analogWrite(pinLed,luz); 
+   delay(300); 
+   valPot2 = analogRead(pinPot2);
+   Serial.println("el sonido es:");
+   Serial.println(valPot2);
+   luz2 = map(valPot2,0,1023,0,255);
+   analogWrite(pinLed2,luz2); 
    delay(300);
 }
